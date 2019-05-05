c2144f9f02355bcde80fd3f1d02702358a888a12
陈康 <kangchen@sf-express.com>
2019-05-05 18:35:49 +0800

    打war包修改

--- multi-module-web/src/main/java/com/ck/multimoduleweb/MultiModuleWebApplication.java
+++ multi-module-web/src/main/java/com/ck/multimoduleweb/MultiModuleWebApplication.java
@@ -3,15 +3,21 @@ package com.ck.multimoduleweb;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.ck.*.mapper"})
 @ComponentScan(basePackages={"com.ck"})
-public class MultiModuleWebApplication {
+public class MultiModuleWebApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MultiModuleWebApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MultiModuleWebApplication.class);
+    }
 }
